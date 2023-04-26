00e3f0bf9d1d83430d54749b7de6675eebb788a3
unknown <ma7351@princeton.edu>
2023-04-26 07:52:11 -0400

    Update solve.py

--- src/modeling_cascading_failure/solve.py
+++ src/modeling_cascading_failure/solve.py
@@ -3,7 +3,7 @@ from scipy.integrate import solve_ivp
 
 
 
-def swing_equation_ODE(t,X_t,K,P,I,gamma):
+def swing_equation_ODE(t,X_t,K_G,K_B,P,I,gamma):
     """
     >The function takes as input:
     
@@ -23,11 +23,12 @@ def swing_equation_ODE(t,X_t,K,P,I,gamma):
             dX_dt[i] = X_t[N+i]
         # Computing the derivative of omega_i
             dX_dt[N+i] = (1/I[i])*(P[i] - gamma[i]*X_t[N+i]
-                       + sum([K[i][j]*np.sin(X_t[j]-X_t[i]) for j in range(N)]))
+                       + sum([K_G[i][j]*np.cos(X_t[j]-X_t[i]) for j in range(N)])
+                       + sum([K_B[i][j]*np.sin(X_t[j]-X_t[i]) for j in range(N)]))
 
     return dX_dt
 
-def simulate_time_step(X_t,K,P,I,gamma,delta_t):
+def simulate_time_step(X_t,K_G,K_B,P,I,gamma,delta_t):
     """
     >The function takes as input:
     
@@ -41,7 +42,7 @@ def simulate_time_step(X_t,K,P,I,gamma,delta_t):
     X_t_plus_1 : np.array of dimension 2N+1 giving thetas and omegas at time t+1
     """
     # Integrating the initial value problem from t to t+delta_t
-    integration = solve_ivp(swing_equation_ODE, (0.0,delta_t), X_t.flatten(),args =(K,P,I,gamma),t_eval= [0,delta_t_test])
+    integration = solve_ivp(swing_equation_ODE, (0.0,delta_t), X_t.flatten(),args =(K_G,K_B,P,I,gamma),t_eval= [0,delta_t])
     #Extracting the state at t+delta_t:
     X_t_plus_1 = integration.y.T[-1]
     return X_t_plus_1
